d0be76332c255002d82a5cbad7715845c43d25f9
Terry <terrymunweihao+github@gmail.com>
2014-01-10 05:17:28 +0100

    First release of StickySort

---
+++ README.md
@@ -0,0 +1,142 @@
+# StickySort
+A jQuery plugin for tables with sticky headers, columns and sortable feature [View demo here](http://terrymun.github.io/StickySort/).
+
+## Introduction
+StickySort is a jQuery plugin that creates sticky headers and columns on your tables, complete with the possibility to add a sortable functionality. This plugin was inspired by a recent task at work where I have to create sticky headers to allow users to orient themselves in large tables that, at many instances, fill the full height of the viewport.
+
+You can [read my article on Codrops](http://tympanus.net/codrops/2014/01/09/sticky-table-headers-columns/) about the basic mechanisms behind the plugin. A lot of calculations are involved, so you have been warned!
+
+Moreover, you can [visit the demo of this plugin](http://terrymun.github.io/StickySort/) on the project page hosted with GitHub.
+
+## Installation
+To install StickySort, you will have to include the following resources in your page. The JS files should be loaded in the order stipulated below. For the CSS file, you can either incorporate it with your site's stylesheet, or load it externally through the `<link>` element in `<head>`.
+
+| Type | File Name              | Description                                                                                                            |
+|------|------------------------|------------------------------------------------------------------------------------------------------------------------|
+| JS   | [jQuery 1.x](http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js) | **External Dependency**: The jQuery 1.x library is needed for Fluidbox functionality.       |
+| JS   | [Ben Alman's throttle/debounce plugin](http://cdnjs.cloudflare.com/ajax/libs/jquery-throttle-debounce/1.1/jquery.ba-throttle-debounce.min.js)           | **External Dependency**: This plugin allows us to throttle or debounce certain events, allowing for better browser performance. |
+| JS   | `jquery.stickysort.js` | Confers the main functionality of Fluidbox. Alternatively, you can load the minified version, `jquery.fluidbox.min.js` |
+| CSS  | `css/stickysort.css`   | Offers styles that are crucial for the correct display of sticky elements. The appearance will break if this is not included. |
+
+## Usage
+### Basic
+It is rather straightforward to use StickySort &mdash; simply chain the `.stickySort()` method to a selector of your choice. Your table should include the following elements:
+
+- A `<thead>` element containing **only** one single `<tr>` populated with **only** `<th>` elements (table headers)
+- A `<tbody>` element containing `<td>` wrapped within one or more `<tr>`
+
+Therefore, a sample table with the most basic usage (only sticky header will be dynamically added) should look as follow:
+
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <!-- add more <th> as of when needed -->
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <!-- add more <td> as of when needed -->
+            </tr>
+            <!-- add more rows as of when needed -->
+        </tbody>
+    </table>
+
+If you would want a sticky column, too, you will need to use `<th>` for the first cells in all `<tr>` elements in your `<tbody>`, i.e.:
+
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <!-- add more <th> as of when needed -->
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th></th><!-- first cell must ne <th> -->
+                <td></td><!-- other cells are <td> -->
+                <!-- add more <td> as of when needed -->
+            </tr>
+            <!-- add more rows as of when needed -->
+        </tbody>
+    </table>
+
+In your JS file, you can simply chain the `.sticktSort()` method to your selector on DOM ready, for example:
+
+    $(function () {
+        $('#content table').stickySort();
+    })
+
+### Configuration
+Fluidbox can be configured according to your needs. The following options are available:
+
+| Option           | Type      | Default value | Description                           |
+|------------------|-----------|---------------|---------------------------------------|
+| `threshold`      | Object    |               | Stores objects pertaining to calculating of which how far from the end of the table should the sticky header disappear. |
+| `threshold.rows` | Numeric   | `3`           | Number of rows, from the bottom of the table where the sticky header will disappear. |
+| `threshold.viewport` | Numeric | `0.25`      | Fraction of the current viewport height, from the bottom of the table where the sticky header will disappear. |
+| `threshold.px`   | Numeric   | `false`       | Pixel value of height, from the bottom of the table where the sticky header will disappear. |
+| `threshold.allowanceEval` | String | `min`   | How the above three parameters should be compared, which the plugin will choose either the smallest (`min`) or the largest (`min`) of the three values to set as the allowance/threshold. |
+| `sortable`       | Boolean   | `false`       | Designates the table as sortable or not. |
+| `scrollThrottle` | Numeric   | `15`          | Throttles the scroll event to relief load on JavaScript engine. 15ms is good enough to be smooth to the eye, yet not too taxing on the browser. |
+| `resizeDebounce` | Numeric   | `250`         | Debounces the window resize event to relief load on JavaScript engine. |
+
+This is how the default options look like:
+
+    {
+        threshold: {
+            rows: 3,
+            viewport: 0.25,
+            px: false,
+            allowanceEval: 'min'
+        },
+        sortable: false,
+        scrollThrottle: 15,
+        resizeDebounce: 250
+    }
+
+----
+
+## Styling
+For your convenience, here is the generated markup when the plugin detects a table in your document and does its magic:
+
+    <div class="sticky-wrap [and other classes copied from original table]">
+        <table class="sticky-enabled [and its original classes]">
+            <!-- table content -->
+        </table>
+        <div class="sticky-col">
+            <table />
+        </div>
+        <div class="sticky-intersect">
+            <table />
+        </div>
+        <div class="sticky-thead">
+            <table />
+        </div>
+    </div>
+
+stickySort will automatically copy the classes you have set on the original table element onto its parent, so as to allow for convenient styling. However, if you are intending the style to only apply to the table element and not its parent (they will share the same classes), you should use the element selector, i.e.
+
+    table.sample-table {
+        /* table specific style that does to apply to wrapping parent */
+    }
+
+----
+
+## Sortable
+The sortable function does not rely on the default `.sort()` function in JavaScript due to its various limitations &mdash; more importantly, it makes it extremely hard to perform human sorting. Therefore, I have adopted [a function written by Brian Huisman back in 2008](http://my.opera.com/GreyWyvern/blog/show.dml/1671288), but still works beautifully today.
+
+### Enabling sorting
+Sorting is disabled by default. In order to enable the sorting function, you can either implement it universally, i.e.:
+
+    $('table').stickySort({ sortable: true });
+
+&hellip;or if you want to only target specific tables, the plugin will recognize tables that has the class 'sortable' or has the HTML5 data- attribute of `data-sortable`, and mark them as available for sorting.
+
+### Sort states
+There are three possible sort states with this plugin, and they can be cycled through by successive clicking. Note that sorting from one column resets the sorting state in the previous column (which is the intuitive pattern, anyway):
+
+1. Default state &mdash; rows are not sorted, and appear as-is from the original markup
+2. Sort ascending &mdash; rows are sorted by ascending order
+3. Sort descending &mdash; rows are sorted by descending order
---
+++ jquery.stickysort.js
@@ -0,0 +1,310 @@
+(function($){
+	$.fn.stickySort = function(opts) {
+
+		// Default settings
+		var settings = $.extend(true, {
+			threshold: {
+				rows: 3,
+				viewport: 0.25,
+				px: false,
+				allowanceEval: 'min'
+			},
+			sortable: false,
+			scrollThrottle: 15,
+			resizeThrottle: 250
+		}, opts);
+
+		this.each(function() {
+			if($(this).is('table') && $(this).find('thead').length > 0 && $(this).find('th').length > 0) {
+				// Clone <thead>
+				var $w	   = $(window),
+					$t	   = $(this),
+					$thead = $t.find('thead').clone(),
+					$col   = $t.find('thead, tbody').clone();
+
+				// Add class, copy children classes, remove margins, reset width and wrap table
+				$t
+				.wrap('<div class="sticky-wrap" />')
+				.parent()
+					.addClass($t.attr('class'))
+					.end()
+				.addClass('sticky-enabled');
+
+				if($t.hasClass('overflow-y')) $t.removeClass('overflow-y').parent().addClass('overflow-y');
+
+				// Create new sticky table head (basic)
+				$t.after('<div class="sticky-thead"><table /></div>');
+
+				// If <tbody> contains <th>, then we create sticky column and intersect (advanced)
+				if($t.find('tbody th').length > 0) {
+					$t.after('<div class="sticky-col"><table /></div><div class="sticky-intersect"><table /></div>');
+				}
+
+				// Create shorthand for things
+				var $stickyHead  = $(this).siblings('.sticky-thead'),
+					$stickyCol   = $(this).siblings('.sticky-col'),
+					$stickyInsct = $(this).siblings('.sticky-intersect'),
+					$stickyWrap  = $(this).parent('.sticky-wrap');
+
+				$stickyHead.find('table').append($thead);
+
+				$stickyCol
+				.find('table')
+				.append($col)
+					.find('thead th:gt(0)').remove()
+					.end()
+					.find('tbody td').remove();
+
+				$stickyInsct.html('<thead><tr><th>'+$t.find('thead th:first-child').html()+'</th></tr></thead>');
+				
+				// Set widths
+				var setWidths = function () {
+						$t
+						.find('thead th').each(function (i) {
+							$stickyHead.find('th').eq(i).width($(this).width());
+						})
+						.end()
+						.find('tr').each(function (i) {
+							$stickyCol.find('tr').eq(i).height($(this).height());
+						});
+
+						// Set width of sticky table head
+						$stickyHead
+						.width($stickyWrap.width())
+						.find('table')
+							.width($t.width());
+
+						// Set width of sticky table col
+						$stickyCol.find('th').add($stickyInsct.find('th')).width($t.find('thead th').width());
+
+						// Set position sticky intersect
+						$stickyCol.find('table').css({
+							left: $stickyWrap.offset().left
+						});
+					},
+					repositionSticky = function () {
+						// Return value of calculated allowance
+						var allowance = calcAllowance();
+					
+						// 1. Deal with positioning of sticky header
+						// Check if wrapper parent is overflowing along the y-axis
+						if($t.height() > $stickyWrap.height()) {
+							// If it is overflowing (advanced layout)
+							// Position sticky header based on wrapper scrollTop()
+							if($stickyWrap.scrollTop() > 0) {
+								// When top of wrapping parent is out of view
+								$stickyHead.add($stickyInsct).css({
+									opacity: 1,
+									position: 'fixed',
+									top: $stickyWrap.offset().top - $w.scrollTop(),
+									left: $stickyWrap.offset().left
+								}).find('table').css({
+									left: -$stickyWrap.scrollLeft()
+								});
+							} else {
+								// When top of wrapping parent is in view
+								$stickyHead.add($stickyInsct).css({
+									opacity: 0
+								});
+							}
+						} else {
+							// If it is not overflowing (basic layout)
+							// Position sticky header based on viewport scrollTop
+							if($w.scrollTop() > $t.offset().top && $w.scrollTop() < $t.offset().top + $t.outerHeight() - allowance) {
+								// When top of viewport is in the table itself
+								$stickyHead.add($stickyInsct).css({
+									opacity: 1,
+									position: 'fixed',
+									left: $stickyWrap.offset().left
+								}).find('table').css({
+									left: -$stickyWrap.scrollLeft()
+								});
+							} else {
+								// When top of viewport is above or below table
+								$stickyHead.add($stickyInsct).css({
+									opacity: 0,
+									position: 'absolute',
+									left: 0
+								});
+							}
+						}
+
+						// 2. Now deal with positioning of sticky column
+						if($stickyWrap.scrollLeft() > 0) {
+							// When left of wrapping parent is out of view
+							$stickyCol.css({
+								position: 'fixed',
+								left: $stickyWrap.offset().left,
+								top: $stickyWrap.offset().top - $w.scrollTop(),
+								height: $stickyWrap.height()
+							})
+							.find('table')
+								.css({
+									top: -$stickyWrap.scrollTop(),
+									left: 0
+								})
+							.end()
+							.add($stickyInsct).css({
+								opacity: 1
+							});
+						} else {
+							// When left of wrapping parent is in view
+							$stickyCol
+							.css({ opacity: 0 });
+						}
+					},
+					calcAllowance = function () {
+						var rowHeight = 0,
+							allowance = [];
+
+						// Calculate allowance
+						$t.find('tbody tr:lt('+settings.threshold.rows+')').each(function () {
+							rowHeight += $(this).height();
+						});
+						allowance.push(rowHeight);
+						
+						// Get height based on viewport
+						allowance.push($w.height()*settings.threshold.viewport)
+						
+						// If pixel threshold exists, add it
+						if(settings.threshold.px) {
+							allowance.push(settings.threshold.px);
+						}
+
+						// Get minimum or maximum?
+						if(settings.threshold.allowanceEval == 'min') {
+							return Math.min.apply(null, allowance);
+						} else {
+							return Math.max.apply(null, allowance);
+						}
+					};
+
+				setWidths();
+
+				$t.parent('.sticky-wrap').scroll($.throttle(settings.scrollThrottle, repositionSticky));
+
+				$w
+				.load(setWidths)
+				.resize($.debounce(settings.resizeThrottle, function () {
+					setWidths();
+					repositionSticky();
+				}))
+				.scroll($.throttle(settings.scrollThrottle, repositionSticky));
+
+				// Extended feature: Sortable table
+				// Do sorting only when original table is slated for sorting:
+				// 1. Check if HTML5 data- attribute, data-sortable, exists
+				// 2. Check if table has the class 'sortable'
+				// 3. Check if settings.sortable is enabled
+				if(settings.sortable || ($t.data('sortable') != undefined || $t.hasClass('sortable'))) {
+
+					// Store original order of rows first, so we can return to its natural/resting state
+					$t.find('tbody tr').each(function (i) {
+						$(this).attr('data-sortOrder', i);
+					});
+
+					$stickyWrap
+					.addClass('sortable')
+					.find('thead th')
+						.addClass('sort-default')
+						.data('sortState', 1)
+						.append('<a href="#" class="sort-handle"></a>')
+						.find('.sort-handle')
+							.click(function(e){ e.preventDefault(); });
+
+					// Bind click function to all <thead>'s <th> elements in the original table AND all dynamically generated sticky tables
+					$stickyWrap.on('click', '.sticky-enabled thead th, .sticky-thead thead th, .sticky-col thead th, .sticky-intersect thead th', function () {
+
+						// Declare some variables
+						var $th      = $(this),
+							colIndex = $th.index(),
+							$thSib   = $stickyWrap.find('thead th').eq(colIndex),
+							$rows    = $.makeArray($t.find('tbody tr'));
+
+						// Decide action - We have 3 states that we want to cater to:
+						// 1. Original state (as how the data has been entered)
+						// 2. Sorted state, ascending
+						// 3. Sorted state, descending
+						if($th.data('sortState')%3 == 0) {
+
+							// Sort rows back to original order
+							$rows.sort(function (a,b) {
+								return parseInt($(a).attr('data-sortOrder')) - parseInt($(b).attr('data-sortOrder'));
+							});
+
+							$th.add($thSib).removeClass().addClass('sort-default');
+
+						} else {
+							// Sort rows
+							// Algorithm adopted from http://my.opera.com/GreyWyvern/blog/show.dml/1671288
+							$rows.sort(function (a, b) {
+
+								var aa = chunkify($(a).children().eq(colIndex).text()),
+									bb = chunkify($(b).children().eq(colIndex).text());
+
+								function chunkify(t) {
+									var tz = [], x = 0, y = -1, n = 0, i, j;
+
+									while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+										var m = (i == 46 || (i >=48 && i <= 57));
+										if (m !== n) {
+											tz[++y] = "";
+											n = m;
+										}
+										tz[y] += j;
+									}
+									return tz;
+								}
+
+								for (x = 0; aa[x] && bb[x]; x++) {
+									if (aa[x] !== bb[x]) {
+										var c = Number(aa[x]), d = Number(bb[x]);
+										if (c == aa[x] && d == bb[x]) {
+											return c - d;
+										} else return (aa[x] > bb[x]) ? 1 : -1;
+									}
+								}
+
+								return aa.length - bb.length;
+							});
+
+							// Reverse order on second click
+							if($th.data('sortState')%3 == 2) {
+								$rows = $rows.reverse();
+								$th.add($thSib).removeClass().addClass('sort-desc');
+							} else {
+								$th.add($thSib).removeClass().addClass('sort-asc');
+							}
+						}
+
+						// Increase state by 1
+						var state = $th.data('sortState')+1;
+
+						// Sync state across all <th> elements
+						$th.add($thSib).data('sortState', state%3);
+
+						// Reset status of other <th>
+						$stickyWrap.find('table').each(function(i) {
+							$(this)
+							.find('thead th').not(':eq('+colIndex+')')
+								.data('sortState', 1)
+								.removeClass().addClass('sort-default');
+						});
+
+						// Add rows
+						$t.find('tbody').html($rows);
+
+						// Sync $stickyCol
+						$t.find('tbody tr').each(function(i) {
+							$stickyCol.find('tbody tr').eq(i).find('th').html($(this).find('th').first().html());
+						});
+
+					});
+				}
+			}
+		});
+
+		// Return to allow chaining
+		return this;
+	};
+})(jQuery);
